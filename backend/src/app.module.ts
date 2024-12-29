9cca57645d9e604ffeeaf9a8f186c71973ca00e6
himanthikavidu <himanthikavidu@gmail.com>
2024-12-29 20:42:22 +0530

    Fronend change Implementation

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -9,7 +9,7 @@ import { ConfigModule } from '@nestjs/config';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT, 10) || 3307,
+      port: parseInt(process.env.DB_PORT, 10) || 3306,
       username: process.env.DB_USERNAME || 'root',
       password: process.env.DB_PASSWORD || 'password',
       database: process.env.DB_DATABASE || 'student_db',
